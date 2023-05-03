ab3bd1b76103321b9ba398ea872e316dacb6d3a0
David <david123rana@gmail.com>
2023-05-03 10:40:02 +0530

    update header name

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import logo from './logo.svg';
+import Page from './components/First Program/Page';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 style={{ color:'red' }}>David Singh Rana</h1>
-        <h2>Updating some fun</h2>
-      </header>
+    <div className='App'>
+      <Page />
     </div>
   );
 }
---
+++ src/components/First Program/Page.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './style.css';
+
+function Page() {
+  return (
+    <>
+
+    <section className="pimg2">
+        <div className="ptext">
+            <span className="textBg">Explore NATURE</span>
+            {/* <span className="textBg">Dev-Guild Meeting</span> */}
+        </div>
+    </section>
+    <section className="section section-dark">
+        <h2>Section First</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam ratione animi, nisi quia eum, rem nulla, culpa pariatur laborum voluptates tempore dolor deleniti tempora sequi tenetur provident ducimus! Nulla inventore nostrum, unde facere exercitationem adipisci obcaecati nisi omnis. Quibusdam eligendi qui natus animi voluptate rerum libero, corporis quod error nulla nisi alias obcaecati consectetur sit culpa quaerat delectus nesciunt enim ratione non iusto sed dolorem? Et eaque omnis nemo reiciendis dolorem repudiandae natus maxime quasi vero alias perferendis architecto nobis harum earum enim ipsum ex distinctio expedita error, dicta illum dolore. Debitis magnam dolorem inventore quaerat consequatur, dolores quam magni.</p>
+    </section>
+    <section className="pimg3">
+        <div className="ptext">
+            <span className="textBg">Look, DEPP INTO NATURE</span>
+        </div>
+    </section>
+    <section className="section section-dark">
+        <h2>Section Second</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam ratione animi, nisi quia eum, rem nulla, culpa pariatur laborum voluptates tempore dolor deleniti tempora sequi tenetur provident ducimus! Nulla inventore nostrum, unde facere exercitationem adipisci obcaecati nisi omnis. Quibusdam eligendi qui natus animi voluptate rerum libero, corporis quod error nulla nisi alias obcaecati consectetur sit culpa quaerat delectus nesciunt enim ratione non iusto sed dolorem? Et eaque omnis nemo reiciendis dolorem repudiandae natus maxime quasi vero alias perferendis architecto nobis harum earum enim ipsum ex distinctio expedita error, dicta illum dolore. Debitis magnam dolorem inventore quaerat consequatur, dolores quam magni.</p>
+    </section>
+    <section className="pimg3">
+        <div className="ptext">
+            <span className="textBg">Look, DEPP INTO NATURE</span>
+        </div>
+    </section>
+    </>
+  )
+}
+
+export default Page
